0289d806e3dad174c73c3893258c8020c6748a1c
Pawandeep123321 <123100974+Pawandeep123321@users.noreply.github.com>
2023-01-22 13:18:26 +0100

    Update rps6.py

--- rps6.py
+++ rps6.py
@@ -1,55 +1,75 @@
+import streamlit as st
 import cv2
 import numpy as np
-import streamlit as st
-# Load the cascade for hand gesture recognition
-hand_cascade = cv2.CascadeClassifier('hand.xml')
 
-# Define the choices for the game
-choices = {0: 'rock', 1: 'paper', 2: 'scissors'}
+st.set_page_config(page_title="Rock Paper Scissors", page_icon=":guardsman:", layout="wide")
 
-# Start the webcam
-cap = st.camera_input("start playing")
+# Create a function to process the webcam image
+def process_image(img):
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-while True:
-# Read a frame from the webcam
-    frame= cap
-
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect hands in the frame
-    hands = hand_cascade.detectMultiScale(gray, 1.1, 5)
+    # Detect the hand using a Haar cascade classifier
+    hand_cascade = cv2.CascadeClassifier("hand.xml")
+    hands = hand_cascade.detectMultiScale(gray, 1.3, 5)
 
     # Draw a rectangle around the hand
     for (x, y, w, h) in hands:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
 
-    # Show the webcam feed
-    cv2.imshow('Webcam', frame)
+    return img
 
-    # Check if the user has made a gesture
-    if len(hands) > 0:
-        # Get the user's choice
-        choice = np.random.randint(0, 3)
+# Create a function to determine the hand gesture
+def get_hand_gesture(img):
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Print the user's choice
-        print(f'You chose {choices[choice]}')
+    # Use thresholding to segment the hand
+    ret, thresh = cv2.threshold(gray, 70, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
-        # Compare the user's choice to the computer's choice
-        computer_choice = np.random.randint(0, 3)
-        if choice == computer_choice:
-            print("It's a tie!")
-        elif (choice == 0 and computer_choice == 2) or \
-             (choice == 1 and computer_choice == 0) or \
-             (choice == 2 and computer_choice == 1):
-            print('You win!')
-        else:
-            print('You lose!')
+    # Find the contours of the hand
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Check if the user wants to quit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-       break
+    # Determine the number of fingers based on the number of defects
+    defects = cv2.convexityDefects(contours[0], hierarchy[0])
+    if defects is not None:
+        fingers = 0
+        for i in range(defects.shape[0]):
+            s, e, f, d = defects[i, 0]
+            start = tuple(contours[0][s][0])
+            end = tuple(contours[0][e][0])
+            far = tuple(contours[0][f][0])
+            fingers += 1
 
+    # Determine the hand gesture based on the number of fingers
+    if fingers == 0:
+        gesture = "rock"
+    elif fingers == 1:
+        gesture = "scissors"
+    else:
+        gesture = "paper"
+
+    return gesture
+
+# Create the main function
+def main():
+    st.title("Rock Paper Scissors")
+
+    # Create a button to start the webcam
+    if st.button("Start webcam"):
+        # Open the webcam
+        cap = cv2.VideoCapture(0)
+
+        while True:
+            # Get the webcam image
+            ret, frame = cap.read()
+
+            # Process the image
+            processed_img = process_image(frame)
+
+            # Display the processed image
 # Release the webcam and close the window
 cap.release()
 cv2.destroyAllWindows()
