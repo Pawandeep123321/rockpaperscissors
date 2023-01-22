cf6cea59b77225989b3d1f0dc0e04682d406194b
Pawandeep123321 <123100974+Pawandeep123321@users.noreply.github.com>
2023-01-22 13:24:53 +0100

    Update rps6.py

--- rps6.py
+++ rps6.py
@@ -1,75 +1,35 @@
 import streamlit as st
-import cv2
-import numpy as np
 
-st.set_page_config(page_title="Rock Paper Scissors", page_icon=":guardsman:", layout="wide")
-
-# Create a function to process the webcam image
-def process_image(img):
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Detect the hand using a Haar cascade classifier
-    hand_cascade = cv2.CascadeClassifier("hand.xml")
-    hands = hand_cascade.detectMultiScale(gray, 1.3, 5)
-
-    # Draw a rectangle around the hand
-    for (x, y, w, h) in hands:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
-
-    return img
-
-# Create a function to determine the hand gesture
-def get_hand_gesture(img):
-    # Convert the image to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Use thresholding to segment the hand
-    ret, thresh = cv2.threshold(gray, 70, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-
-    # Find the contours of the hand
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Determine the number of fingers based on the number of defects
-    defects = cv2.convexityDefects(contours[0], hierarchy[0])
-    if defects is not None:
-        fingers = 0
-        for i in range(defects.shape[0]):
-            s, e, f, d = defects[i, 0]
-            start = tuple(contours[0][s][0])
-            end = tuple(contours[0][e][0])
-            far = tuple(contours[0][f][0])
-            fingers += 1
-
-    # Determine the hand gesture based on the number of fingers
-    if fingers == 0:
-        gesture = "rock"
-    elif fingers == 1:
-        gesture = "scissors"
-    else:
-        gesture = "paper"
-
-    return gesture
-
-# Create the main function
 def main():
-    st.title("Rock Paper Scissors")
+    st.title("Rock Paper Scissors Game")
+    st.write("Make a hand gesture for Rock, Paper, or Scissors and press the button to detect.")
 
-    # Create a button to start the webcam
-    if st.button("Start webcam"):
-        # Open the webcam
-        cap = cv2.VideoCapture(0)
+    # Start the webcam
+    webcam = st.empty()
+    webcam.video(streaming=True)
 
-        while True:
-            # Get the webcam image
-            ret, frame = cap.read()
+    # Get the user's gesture
+    gesture = st.button("Detect Gesture")
 
-            # Process the image
-            processed_img = process_image(frame)
+    if gesture:
+        # Get the webcam image
+        img = webcam.get_image()
 
-            # Display the processed image
-# Release the webcam and close the window
-        cv2.release()
- cv2.destroyAllWindows()
+        # Use image processing to detect the number of fingers
+        # (you would need to write your own image processing code here)
+        fingers = detect_fingers(img)
+
+        # Determine the gesture based on the number of fingers
+        if fingers == 0:
+            gesture = "Rock"
+        elif fingers == 1:
+            gesture = "Paper"
+        elif fingers == 2:
+            gesture = "Scissors"
+        else:
+            gesture = "Invalid gesture"
+
+        st.success("Gesture detected: " + gesture)
+
+if _name_ == "_main_":
+    main()
